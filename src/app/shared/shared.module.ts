46d1aa42c2b491522c85ee3250e1afe0780bc24d
Jahongir780110 <jahongir780110@gmail.com>
2022-11-28 14:54:13 +0500

    feat: add desktop app capabilities

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -14,7 +14,7 @@ import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.componen
 import { TrimDirective } from './trim.directive';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
 }
 
 @NgModule({
